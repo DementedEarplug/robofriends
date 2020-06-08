3c38fbe0a20ecea8e911615a17e26af148006e05
DementedEarplug <dementedearplug@pop-os.localdomain>
2020-06-08 13:47:13 -0400

    Added a dataset of robot names, added a robots card component, deleted hello world component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 import "./components/RoboCard"
-import HelloWorld from "./components/RoboCard";
+import RoboCard from "./components/RoboCard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld greeting={'Wassup manjarino!'} />
+        <RoboCard 
+          img='./logo.svg'
+          roboName={'Wassup manjarino!'} 
+          roboEmail={'some@email.com'}
+        />
+        <RoboCard 
+          img='./logo.svg'
+          roboName={'Shemanicaster'} 
+          roboEmail={'some@email.com'}
+        />
+        <RoboCard 
+          img='./logo.svg'
+          roboName={'Tipromarinki'} 
+          roboEmail={'some@email.com'}
+        />
       </div>
     );
   }
--- src/components/RoboCard.jsx
+++ src/components/RoboCard.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import '../style/RoboCard.css'
-
+import "tachyons"
 export default class HelloWorld extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello Worl my mein!!</h1>
-        <p>{this.props.greeting}</p>
+      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img src={`https://robohash.org/${this.props.roboName}?200x200`} alt='Robo Img'/>
+        <h1>{this.props.roboName}</h1>
+        <p>{this.props.roboEmail}</p>
       </div>
     );
   }
