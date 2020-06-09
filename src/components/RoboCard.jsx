bc2199876a0065fafce613ec3fa29b22ac613586
DementedEarplug <dementedearplug@pop-os.localdomain>
2020-06-09 10:20:04 -0400

    Changed card background color

--- src/components/RoboCard.jsx
+++ src/components/RoboCard.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "../style/RoboCard.css"
 import "tachyons"
 export default class HelloWorld extends Component {
   render() {
     return (
-      <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <div className="roboCard dib br3 pa3 ma2 grow bw2 shadow-5">
         <img src={`https://robohash.org/${this.props.roboName}?200x200`} alt='Robo Img'/>
         <h1>{this.props.roboName}</h1>
         <p>{this.props.roboEmail}</p>
